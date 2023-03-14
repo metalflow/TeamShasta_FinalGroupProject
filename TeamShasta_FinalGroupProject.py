f3454cac8048a2fb4afab7d8ee7f66f2900fb6e0
Cole Risch <metal.flow@gmail.com>
2023-03-13 20:31:21 -0700

    My first CNN tutorial complete

--- TeamShasta_FinalGroupProject.py
+++ TeamShasta_FinalGroupProject.py
@@ -1,5 +1,14 @@
 #imports
 import tensorflow as tf
+from tensorflow import keras
+from keras.datasets import fashion_mnist
+from keras.utils import np_utils
+from keras.layers import Dense, Dropout, Activation, Flatten
+from keras.layers import Convolution2D, MaxPooling2D
+from keras.models import Sequential
+from matplotlib import pyplot as plt
+import numpy as np
+np.random.seed(123)  # for reproducibility
 
 #constants
 
@@ -8,5 +17,52 @@ import tensorflow as tf
 #classes
 
 #main program
+#load dataset
+(x_train,y_train),(x_test,y_test)=fashion_mnist.load_data()
+print("x_train")
+print(x_train.shape)
+plt.imshow(x_train[0])
+#reshape the tensor to specify a single color channel (grayscale)
+print("x_train reshape")
+x_train = x_train.reshape(x_train.shape[0], 28, 28, 1)
+x_test = x_test.reshape(x_test.shape[0], 28, 28, 1)
+print(x_train.shape)
+#normalize values to floats between 0 and 1
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
+x_train /= 255
+x_test /= 255
+
+print("y_train")
+print(y_train.shape)
+print("first 10 items in y_train")
+print( y_train[:10] )
+print("y_train reshape")
+# Convert 1-dimensional class arrays to 10-dimensional class matrices
+y_train = np_utils.to_categorical(y_train, 10)
+y_test = np_utils.to_categorical(y_test, 10)
+print(y_train.shape)
+
+#begin creating model
+model = Sequential()
+model.add(Convolution2D(32, (3,3), activation='relu', input_shape=(28,28,1)))
+print( model.output_shape )
+model.add(Convolution2D(32, (3,3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(Dropout(0.25))
+model.add(Flatten())
+model.add(Dense(128, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(10, activation='softmax'))
+model.compile(loss='categorical_crossentropy',
+              optimizer='adam',
+              metrics=['accuracy'])
+#begin training
+model.fit(x_train, y_train,batch_size=32, epochs=10, verbose=1)
+
+print("score")
+score = model.evaluate(x_test, y_test, verbose=1)
+print(score)
+
 
 #clean up
